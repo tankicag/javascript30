76d37e3f126b830c39a465d2dc0d4a8835877256
GTango <gonevatankica@gmail.com>
2021-01-11 22:26:16 +0000

    Countdown Timer in progress

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,27 @@
+let countdown;
+const timerDisplay = document.querySelector(".display__time-left");
+
+function timer(seconds) {
+  clearInterval(countdown);
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft <= 0) {
+      clearInterval(countdown);
+      return;
+    }
+
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes} : ${remainderSeconds}`;
+  timerDisplay.textContent = display;
+  console.log({ minutes, remainderSeconds });
+}
